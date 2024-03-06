2a5e30c38dc79d65cffd47377e6165d225332201
Jonathan <51540487+asusplayer@users.noreply.github.com>
2024-03-06 13:20:50 +0100

    saving progress

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -13,6 +13,8 @@ const POINTS_LABEL_SCENE = preload("res://Scenes/points_label.tscn")
 
 @export var enemy_no_collision_frames = 0
 
+var already_hit = false
+
 
 func _process(delta):
 		
@@ -40,20 +42,22 @@ func die():
 	animated_sprite_2d.play("dead")
 	
 func die_from_hit():
-	set_collision_layer_value(3, false)
-	set_collision_mask_value(3, false)
-	
-	rotation_degrees = 180
-	vertical_speed = 0
-	horizontal_speed = 0
-	
-	var die_tween = get_tree().create_tween()
-	die_tween.tween_property(self, "position", position + Vector2(0, -25), .2)
-	die_tween.chain().tween_property(self, "position", position + Vector2(0, 500), 4)
-	
-	var points_label = POINTS_LABEL_SCENE.instantiate()
-	points_label.position = self.position + Vector2(-20, -20)
-	get_tree().root.add_child(points_label)
+	if !already_hit:
+		set_collision_layer_value(3, false)
+		set_collision_mask_value(3, false)
+		already_hit = true
+		
+		rotation_degrees = 180
+		vertical_speed = 0
+		horizontal_speed = 0
+		
+		var die_tween = get_tree().create_tween()
+		die_tween.tween_property(self, "position", position + Vector2(0, -25), .2)
+		die_tween.chain().tween_property(self, "position", position + Vector2(0, 500), 4)
+		
+		var points_label = POINTS_LABEL_SCENE.instantiate()
+		points_label.position = self.position + Vector2(-20, -20)
+		get_tree().root.add_child(points_label)
 	
 func _on_area_entered(area):
 	if area is Koopa and (area as Koopa).in_a_shell and (area as Koopa).horizontal_speed != 0:
